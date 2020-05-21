4fd86528a10371a940bb17a9f31fc35dafe9ac86
Jakarea <jakareaparvez@gmail.com>
2020-05-21 13:18:43 +0600

    Initial out subtree commit gh-pages

---
+++ _next/static/iSSUKDx0SkJccNIdYwhhe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/about":[a],"/blog":[a],"/contact":[a],"/index":[a],"/projects":[a],"/services":[a]}}("static\u002Fchunks\u002Fc723b6085ead95a7725da89ede114f15f63902ef.7875563479062202c845.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
